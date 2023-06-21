16e23aca1e53acdc51f55d66dc0ddaf9ff28c42c
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-06-22 02:10:52 +0530

    PROFILE SECTION: Done ✅

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Socials from './components/ProflieSection/Socials';
 export default function Home() {
 	return (
 		<section className="flex justify-center items-center">
-			<section className="border-2 border-red-500 w-[30%] h-[200vh] flex flex-col items-center">
+			<section className="border-r-2 border-[#768390] w-[30%] h-[200vh] flex flex-col items-center invisible">
 				<About />
 				<div className="my-3 w-[300px] border-[1px] border-[#768390]"></div>
 				<Socials />
